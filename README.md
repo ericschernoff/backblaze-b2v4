f318a2d44014884cb70482af301e29ebc1526c66
Eric Chernoff <eric@weaverstreet.net>
2026-05-10 20:24:24 +0000

    Minil init

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Backblaze::B2V4 - It's new $module
+
+# SYNOPSIS
+
+    use Backblaze::B2V4;
+
+# DESCRIPTION
+
+Backblaze::B2V4 is ...
+
+# LICENSE
+
+Copyright (C) Eric Chernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Eric Chernoff <eric@weaverstreet.net>
---
+++ lib/Backblaze/B2V4.pm
@@ -0,0 +1,39 @@
+package Backblaze::B2V4;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Backblaze::B2V4 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Backblaze::B2V4;
+
+=head1 DESCRIPTION
+
+Backblaze::B2V4 is ...
+
+=head1 LICENSE
+
+Copyright (C) Eric Chernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Eric Chernoff E<lt>eric@weaverstreet.netE<gt>
+
+=cut
+
