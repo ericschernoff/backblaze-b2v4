47ad856753bb37c95542f52d6eef8d53b0d295c2
Eric Chernoff <eric@weaverstreet.net>
2026-05-31 23:21:28 -0700

    Doc tweak for bucket maker

--- lib/Backblaze/B2V4.pm
+++ lib/Backblaze/B2V4.pm
@@ -1037,6 +1037,9 @@ Accepts named args:
 
 See: https://www.backblaze.com/b2/docs/b2_create_bucket.html
 
+Note that B2 bucket namess must be unique system-wide, not just your account. 
+Select a name that willbe unique globally.
+
 Example:
 
 	my $success = $b2->b2_bucket_maker(
